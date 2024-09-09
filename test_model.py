e7aa8a340d9df9a4ef0ffe6e20b852c6bc149950
Fernando Paiva <fernandosjp@gmail.com>
2024-09-08 21:58:15 -0300

    Add simple tests

--- test_model.py
+++ test_model.py
@@ -1,6 +1,18 @@
+import pytest
 from utils.transaction_classifier import classify_transaction
 
+transaction_categories = {
+    "Habibs": "Restaurants",
+    "Gasolina": "Transporte",
+    "Estacionamento": "Transporte",
+    "Lanche": "Comida",
+    "Carrefour": "Mercado",
+    "Shopper": "Mercado"
+}
 
-def test_model():
-    description = "Habibs"
-    assert classify_transaction(description) == "Restaurants"
+@pytest.mark.parametrize("description", transaction_categories.keys())
+def test_basic_predictions(description):
+    assert classify_transaction(description) == transaction_categories.get(description, "")
+
+
+# test against model performance metric and a bigger test csv
